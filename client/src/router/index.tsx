60873b1ae906c1e0ba8e7bb092651309b91c2ff5
Tao <ginadrcoder@gmail.com>
2025-05-18 17:31:31 +1000

    correct jittery mobile scrolling

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,27 +1,51 @@
 import { Routes, Route, useLocation } from 'react-router';
-
 import gsap from 'gsap';
 import { ScrollSmoother } from 'gsap/ScrollSmoother';
 import { useGSAP } from '@gsap/react';
-
-// import Boxes from '../pages/Boxes';
+import { useEffect, useState } from 'react';
 import Images from '../pages/Images';
 
 gsap.registerPlugin(ScrollSmoother, useGSAP);
 
 export default function Router() {
   const location = useLocation();
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Detect if the device is mobile
+  useEffect(() => {
+    const checkMobile = () => {
+      // Simple detection based on screen width
+      const mobile = window.innerWidth <= 768;
+      setIsMobile(mobile);
+    };
+
+    // Check on initial load
+    checkMobile();
+
+    // Check on resize
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
 
   useGSAP(() => {
-    ScrollSmoother.create({
-      smooth: 1,
-      effects: true,
-    });
-  }, [location]);
+  const smoother = !isMobile 
+    ? ScrollSmoother.create({
+        smooth: 1,
+        effects: true,
+      })
+    : null;
+  
+  return () => {
+    if (smoother) {
+      smoother.kill();
+    }
+  };
+}, [location, isMobile]);
 
   return (
     <div id="smooth-wrapper">
-      <div id="smooth-content">
+      <div id={isMobile ? "" : "smooth-content"}>
         <Routes>
           <Route>
             <Route index element={<Images />} />
