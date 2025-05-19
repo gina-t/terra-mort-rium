e7f95caf6a24f51bea529e79433dca338ddcf733
Tao <ginadrcoder@gmail.com>
2025-05-19 15:55:07 +1000

    modify index.tsx, add read-more-poetry button, route, page

---
+++ client/src/pages/Poetry.tsx
@@ -0,0 +1,47 @@
+
+const Poetry = () => {
+  return (
+    <div>
+      When great souls die, <br />
+          the air around us becomes <br />
+          light, rare, sterile. <br />  
+          We breathe, briefly. <br />
+          Our eyes, briefly, <br />
+          see with <br />
+          a hurtful clarity. <br />
+          Our memory, suddenly sharpened, <br />
+          examines, <br />
+          gnaws on kind words <br />
+          unsaid, <br />
+          promised walks <br />
+          never taken. <br /><br />
+          Great souls die and <br />
+          our reality, bound to <br />
+          them, takes leave of us. <br />
+          Our souls, <br />
+          dependent upon their <br />
+          nurture, <br />
+          now shrink, wizened. <br />
+          Our minds, formed <br />
+          and informed by their <br />
+          radiance, fall away. <br />
+          We are not so much maddened <br />
+          as reduced to the unutterable ignorance of <br />
+          dark, cold <br />
+          caves. <br /><br />
+          And when great souls die, <br />
+          after a period peace blooms, <br />
+          slowly and always <br />
+          irregularly. Spaces fill <br />
+          with a kind of <br />
+          soothing electric vibration. <br />
+          Our senses, restored, never <br />
+          to be the same, whisper to us. <br />
+          They existed. They existed. <br />
+          We can be. Be and be <br />
+          better. For they existed.
+    </div>
+  )
+}
+
+export default Poetry
